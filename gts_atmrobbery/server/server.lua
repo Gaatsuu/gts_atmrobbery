2b38e4704e2392bd653f3c0e92b678cd17ac8e52
Gaatsuu <natael37100@gmail.com>
2024-08-14 10:47:39 +0200

    Update server.lua

--- gts_atmrobbery/server/server.lua
+++ gts_atmrobbery/server/server.lua
@@ -14,11 +14,23 @@ local function getNumberOfCops()
     return cops
 end
 
+local lastHackAttempt = {}
+
+local COOLDOWN_TIME = Config.Cooldown -- Temps en secondes (30 minutes)
+
 RegisterNetEvent('gts_atmrobbery:tryHack')
 AddEventHandler('gts_atmrobbery:tryHack', function()
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-    
+    local playerId = xPlayer.identifier
+    local currentTime = os.time()
+
+    if lastHackAttempt[playerId] and currentTime - lastHackAttempt[playerId] < COOLDOWN_TIME then
+        local remainingTime = COOLDOWN_TIME - (currentTime - lastHackAttempt[playerId])
+        TriggerClientEvent('esx:showNotification', src, ('Vous devez attendre ~r~%s~s~ avant de pouvoir tenter un autre braquage.'):format(math.ceil(remainingTime / 60) .. ' minutes'))
+        return
+    end
+
     if xPlayer then
         local copsOnline = getNumberOfCops()
         
@@ -26,18 +38,19 @@ AddEventHandler('gts_atmrobbery:tryHack', function()
             local hasItem = xPlayer.getInventoryItem(Config.Item).count
             print(hasItem)
             if hasItem >= 1 then
+                lastHackAttempt[playerId] = currentTime -- Met à jour le dernier temps de tentative
                 local coords = GetEntityCoords(GetPlayerPed(src))
                 xPlayer.removeInventoryItem(Config.Item, 1)
                 TriggerClientEvent('gts_atmrobbery:startHack', src)
             else
                 TriggerClientEvent('gts_atmrobbery:missingItem', src)
-                
             end
         else
             TriggerClientEvent('gts_atmrobbery:notEnoughCops', src)
         end
     end
 end)
+
 
 RegisterNetEvent('gts_atmrobbery:giveMoney')
 AddEventHandler('gts_atmrobbery:giveMoney', function(amount)
